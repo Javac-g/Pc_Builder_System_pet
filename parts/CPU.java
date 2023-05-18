512873826fe2dc16680830e8b64bfe29f03ad22c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-05-18 15:57:13 +0300

    First test

---
+++ parts/CPU.java
@@ -0,0 +1,9 @@
+package parts;
+
+public class CPU {
+    private Long id;
+    private Integer tdp;
+    private Integer core_count;
+    private String socket_type;
+    private Integer performance_core_clock;
+}
